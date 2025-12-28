66979d0941cdab62c122d4e10434521040eb5ef9
Luis Castillo <cncompusistema@gmail.com>
2025-12-28 14:14:34 -0400

    acualizar google tag

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,11 +8,9 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://mejorfibraoptica.site/",
+  site: "https://internetzone.vercel.app/",
   integrations: [react(), tailwind(), sitemap(), icon()],
   redirects: {
-    '/protege-tu-red-guia-paso-a-paso-para-configurar-opendns-en-tu-router-wi-fi/': '/protege-tu-red-configura-opendns/',
-    '/proveedor/': '/',
-    '/buscar-por-ciudad/': '/internet-en/',
+
   }
 });
